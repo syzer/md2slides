cb2f4d0ab57e66a043df41ea37eec6ea05a8656e
syzer <syzer3@gmail.com>
2015-03-26 19:21:49 +0100

    printing usage when executed without parameters

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,15 @@
 #!/usr/bin/env node
 
+var file;
 
-var file = '';
-
-if (process.argv.length > 2) {
+if (process.argv.length <= 2) {
+    console.log('md2slides, usage:');
+    console.log('-----------------');
+    console.log('md2slides inputFile.md > outputFile.html');
+    return;
+} else {
     file = process.argv[2];
 }
-
 
 var app = require('../app')();
 app.init(file);
